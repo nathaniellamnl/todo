bf31341d54ca484c47c8aee6eb7dafbd49456dac
Wan Lam <wlam42@gatech.edu>
2023-12-12 16:37:01 +0800

    Add ToDoItem

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Todo } from "../../types";
+import styles from "./TodoItem.css";
+
+interface TodoItemProps {
+  todo: Todo;
+  toggleTodo: (id: string) => void;
+}
+
+const TodoItem = ({ todo, toggleTodo }: TodoItemProps) => {
+  return (
+    <li className={todo.completed && styles.completed}>
+      {todo.text}
+      <button onClick={() => toggleTodo(todo.id)}>
+        {todo.completed ? "Mark as incomplete" : "Mark as complete"}
+      </button>
+    </li>
+  );
+};
+
+export default TodoItem;
